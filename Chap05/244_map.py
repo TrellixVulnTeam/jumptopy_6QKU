63d3303a8e27c5e20cb539758a08a57bb1ddd361
tjddn7474 <tjddn7474@naver.com>
2017-12-01 10:46:58 +0900

    인사 대상 변경

---
+++ Chap05/244_map.py
@@ -0,0 +1,9 @@
+def two_times(x): return x*2
+
+result = list(map(two_times,[1,2,3,4]))
+
+#for element in result:
+#for element in list(map(two_times,[1,2,3,4])):
+#    print(element)
+#print(result)
+print(list(map(two_times,[1,2,3,4])))
---
+++ CodingDojang/common_multiple.py
@@ -0,0 +1,5 @@
+index  = 1
+while index<=1000:
+    if index % 3 == 0 and index % 5 ==0 :
+        print(index)
+    index+=1
