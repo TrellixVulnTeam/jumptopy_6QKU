549fe04a1bed5857237dbd1bd62af20553679573
hklee <hklee0724@gmail.com>
2018-02-07 17:19:46 +0900

    인사 대상 변경

--- Data_Science/Analysis/전방탐색.py
+++ Data_Science/Analysis/전방탐색.py
@@ -2,8 +2,7 @@ import re
 p = re.compile(".+:")
 m = p.search("http://google.com")
 print(m.group())
-p = re.compile("\+\w :")
-m = p.search("+d :dfsf")
+
+p = re.compile(".+(?=:)") #':'는 소모되지 않는다.
+m = p.search("http://google.com")
 print(m.group())
-m = p.search("d :dfsf")
-print(m)
